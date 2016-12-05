855964510f8333608d25848f80b712aae427e0a6
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2016-12-04 22:17:02 -0500

    Can edit an item, make changes, save.  Uses the renderer to model the render

--- src/renderer.js
+++ src/renderer.js
@@ -35,8 +35,106 @@
   }
 
 
+  // Factory Function
+  // Returns a renderedOrder which is used to control
+  //  the UI
+  // Pass in the order and the target JQuery table element
+  var renderedOrder = function(order, targetTable) {
+    var that = {
+      order: order,
+      targetTable: targetTable
+    };
+
+    // Private Variables
+    var tableBody = targetTable.find('tbody');
+    var generateStaticRow = function(lineItemIndex){
+      var lineItem = that.order.lineItems[lineItemIndex];
+      var newRowObj = $('<tr>');
+      newRowObj.lineItem = lineItem;
+      newRowObj.append($('<td>').append($('<span>').html(lineItemIndex)));
+      newRowObj.append($('<td>').append($('<span>').html(lineItem.description)));
+      newRowObj.append($('<td>').append($('<span>').html(lineItem.ASIN)));
+      newRowObj.append($('<td>').append($('<span>').html(lineItem.item.cost.price)));
+      newRowObj.append($('<td>').append($('<span>').html(lineItem.item.cost.shipping)));
+      newRowObj.append($('<td>').append($('<span>').html(lineItem.item.cost.tax)));
+      newRowObj.append($('<td>').append($('<span>').html(lineItem.quantity)));
+      // make the edit link
+      var editLink = $('<a>');
+      editLink.attr('class', 'btn btn-sm btn-default glyphicon glyphicon-edit');
+      editLink.editThisRow = function(){ that.editRow(lineItemIndex) };
+      editLink.click(editLink.editThisRow);
+      newRowObj.append($('<td>').append($('<span>').append(editLink)));
+
+      return newRowObj;
+    };
+    var generateEditRow = function(lineItemIndex){
+      var lineItem = that.order.lineItems[lineItemIndex];
+      var newRowObj = $('<tr>').addClass('danger');
+      newRowObj.lineItem = lineItem;
+      // make the cancel link
+      var cancelLink = $('<a>');
+      cancelLink.attr('class', 'btn btn-sm btn-danger glyphicon glyphicon-remove');
+      cancelLink.abortEdit = function(){ that.refreshTable() };
+      cancelLink.click(cancelLink.abortEdit);
+
+      newRowObj.append($('<td>').append($('<span>').append(cancelLink)));
+      newRowObj.append($('<td>').append($('<span>').append($('<input>').attr('class','description focused form-control').attr('type','text').attr('value',lineItem.description))));
+      newRowObj.append($('<td>').append($('<span>').append($('<input>').attr('class','ASIN form-control').attr('type','text').attr('value',lineItem.ASIN))));
+      newRowObj.append($('<td>').append($('<span>').append($('<input>').attr('class','price form-control').attr('type','text').attr('value',lineItem.item.cost.price))));
+      // newRowObj.append($('<td>').append($('<span>').html(lineItem.item.cost.shipping)));
+      // newRowObj.append($('<td>').append($('<span>').html(lineItem.item.cost.tax)));
+      newRowObj.append($('<td>').append($('<span>').html('')));
+      newRowObj.append($('<td>').append($('<span>').html('')));
+      newRowObj.append($('<td>').append($('<span>').append($('<input>').attr('class','quantity form-control').attr('type','text').attr('value',lineItem.quantity))));
+
+      // make the edit link
+      var editLink = $('<a>');
+      editLink.attr('class', 'btn btn-sm btn-success glyphicon glyphicon-ok-circle');
+      editLink.saveThisRow = function(){ that.saveRow(newRowObj) };
+      editLink.click(editLink.saveThisRow);
+      newRowObj.append($('<td>').append($('<span>').append(editLink)));
+
+      return newRowObj;
+    };
+    // Public Methods
+    that.refreshTable = function() {
+      tableBody.empty();
+      for (var i = 0; i < order.lineItems.length; i++){
+        tableBody.append(generateStaticRow(i));
+      }
+    };
+    that.editRow =  function(i) {
+
+      var editRow = generateEditRow(i);
+      tableBody.find('tr').eq(i).replaceWith(editRow);
+      editRow.find('.focused').focus();
+      // editRow.hide();
+      // tableBody.append(editRow);
+      // console.log(editRow.parent());
+      // rowToEdit.replaceWith(editRow);
+    };
+    that.saveRow = function(rowToSave){
+      var lineItem = rowToSave.lineItem;
+      var newDesc = rowToSave.find('.description').val();
+      var newASIN = rowToSave.find('.ASIN').val();
+      var newPrice = rowToSave.find('.price').val();
+      var newQty = rowToSave.find('.quantity').val();
+      lineItem.item.description = newDesc;
+      lineItem.item.ASIN = newASIN;
+      lineItem.item.price = newPrice;
+      lineItem.quantity = newQty;
+
+      that.refreshTable();
+    };
+
+    return that;
+  };
+
+
+
   window.lineItemRenderer = {
     renderEditLineItem: renderEditLineItem,
-    renderExistingLineItem: renderExistingLineItem
+    renderExistingLineItem: renderExistingLineItem,
+    renderedOrder: renderedOrder
   }
 })();
